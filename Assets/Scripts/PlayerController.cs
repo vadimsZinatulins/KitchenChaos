2a82ee0e4e3ff652f27397030e2188157616314a
vadimszinatulins <vadimz2@hotmail.com>
2023-02-06 02:20:34 +0000

    Added basic collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,9 +19,34 @@ public class PlayerController : MonoBehaviour {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
 
+        float playerHeight = 2f;
+        float playerRadius = 0.7f;
+        float moveDistance = movementSpeed * Time.deltaTime;
+
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if(!canMove) {
+            Vector3 moveDirX = new Vector3(moveDirection.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if(canMove) {
+                moveDirection = moveDirX;
+            } else {
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if(canMove) {
+                    moveDirection = moveDirZ;
+                }
+            }
+        }
+
+        if(canMove) {
+            transform.position += moveDirection * moveDistance;
+        }
+
         IsWalking = moveDirection != Vector3.zero;
         
-        transform.position += moveDirection * movementSpeed * Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, rotationSpeed * Time.deltaTime);
     }
 }
