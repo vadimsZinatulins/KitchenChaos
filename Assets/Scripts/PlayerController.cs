59711c3062113182a4d1f54f4c94e6df212c73db
Vadims Zinatlins <43823259+vadimsZinatulins@users.noreply.github.com>
2023-02-06 00:47:17 +0000

    Added character controller (#2)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed = 5f;
+
+    void Update() {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if(Input.GetKey(KeyCode.W)) {
+            inputVector.y += 1;
+        }
+        
+        if(Input.GetKey(KeyCode.A)) {
+            inputVector.x -= 1;
+        }
+        
+        if(Input.GetKey(KeyCode.S)) {
+            inputVector.y -= 1;
+        }
+        
+        if(Input.GetKey(KeyCode.D)) {
+            inputVector.x += 1;
+        }
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
+
+        transform.position += moveDirection * movementSpeed * Time.deltaTime;
+    }
+}
