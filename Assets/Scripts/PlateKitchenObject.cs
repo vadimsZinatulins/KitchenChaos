75d130d9ec50a6a002f0245b5e539b6e03491083
vadimszinatulins <vadimz2@hotmail.com>
2023-02-08 23:16:52 +0000

    Added visuals to the plate

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,8 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
     private List<KitchenObjectSO> kitchenObjectSOList;
 
@@ -17,6 +23,11 @@ public class PlateKitchenObject : KitchenObject {
         }
 
         kitchenObjectSOList.Add(kitchenObjectSO);
+
+        OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs { 
+            kitchenObjectSO = kitchenObjectSO
+        });
+
         return true;
     }
 }
